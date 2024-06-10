3787050100e7e96e1e81e2e54a82048801f2b464
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-09 22:26:23 -0600

    #6: adds index files

---
+++ src/schema/index.ts
@@ -0,0 +1,6 @@
+export * from '../../../objectified/objectified-codegen/src/schema/ApiSpec';
+export * from '../../../objectified/objectified-codegen/src/schema/Components';
+export * from '../../../objectified/objectified-codegen/src/schema/Info';
+export * from '../../../objectified/objectified-codegen/src/schema/Path';
+export * from '../../../objectified/objectified-codegen/src/schema/Response';
+export * from '../../../objectified/objectified-codegen/src/schema/Schema';
