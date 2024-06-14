327278479a42ea61ffaa73456197d9766b481249
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-13 22:18:42 -0600

    #10: Created ValidationError

---
+++ src/ValidationError.ts
@@ -0,0 +1,6 @@
+export class ValidationError extends Error {
+  constructor(message: string, asserter=undefined) {
+    super(message);
+    Error.captureStackTrace?.(this, asserter || this.constructor);
+  }
+}
