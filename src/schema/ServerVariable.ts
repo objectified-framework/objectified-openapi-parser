b9af6d2b0042728fe22b94d58722b5ee489bc5e0
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-18 22:06:03 -0600

    #15: adds server variable schema object

---
+++ src/schema/ServerVariable.ts
@@ -0,0 +1,18 @@
+// Covers 4.8.6.1
+export class ServerVariable {
+  private _enum: [string];
+  private _default: string;
+  private _description: string;
+
+  constructor() {
+    this._enum = [];
+  }
+
+  public getEnum = (): [string] => this._enum;
+  public getDefault = (): string => this._default;
+  public getDescription = (): string => this._description;
+
+  public setEnum = (_enum: [string]) => (this._enum = _enum);
+  public setDefault = (def: string) => (this._default = def);
+  public setDescription = (description: string) => (this._description = description);
+}
