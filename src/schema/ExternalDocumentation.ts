9e66e356ac9aad27583de394af7598afaa3b4d21
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-18 22:19:11 -0600

    #20: creates external documentation schema object

---
+++ src/schema/ExternalDocumentation.ts
@@ -0,0 +1,13 @@
+// Covers 4.8.11.1
+export class ExternalDocumentation {
+  private _description: string;
+  private _url: string; // Required
+
+  constructor() {}
+
+  public getDescription = (): string => this._description;
+  public getUrl = (): string => this._url;
+
+  public setDescription = (description: string) => (this._description = description);
+  public setUrl = (url: string) => this._url;
+}
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -4,3 +4,4 @@ export * from './License';
 export * from './ServerVariable';
 export * from './Server';
 export * from './Reference';
+export * from './ExternalDocumentation';
