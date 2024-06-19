a02893ba35cfc9fb39f556ae7d638022ca3eaf67
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-18 22:01:09 -0600

    #11: creates info schema

---
+++ src/schema/Info.ts
@@ -0,0 +1,33 @@
+import { Contact, License } from '.';
+
+// Covers 4.8.2.1
+export class Info {
+  private _title: string; // Required
+  private _summary: string;
+  private _description: string;
+  private _termsOfService: string;
+  private _contact: Contact;
+  private _license: License;
+  private _version: string; // Required
+
+  constructor() {
+    this._contact = new Contact();
+    this._license = new License();
+  }
+
+  public getTitle = (): string => this._title;
+  public getSummary = (): string => this._summary;
+  public getDescription = (): string => this._description;
+  public getTermsOfService = (): string => this._termsOfService;
+  public getContact = (): Contact => this._contact;
+  public getLicense = (): License => this._license;
+  public getVersion = (): string => this._version;
+
+  public setTitle = (title: string) => (this._title = title);
+  public setSummary = (summary: string) => (this._summary = summary);
+  public setDescription = (description: string) => (this._description = description);
+  public setTermsOfService = (termsOfService: string) => (this._termsOfService = termsOfService);
+  public setContact = (contact: Contact) => (this._contact = contact);
+  public setLicense = (license: License) => (this._license = license);
+  public setVersion = (version: string) => (this._version = version);
+}
---
+++ src/schema/index.ts
@@ -0,0 +1,3 @@
+export * from './Contact';
+export * from './Info';
+export * from './License';
