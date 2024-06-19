32a3019ff8e601d0aeaf93dc2e98450f391f8c09
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-18 22:12:47 -0600

    #14: creates server schema object

---
+++ src/schema/Server.ts
@@ -0,0 +1,25 @@
+import { ServerVariable } from '.';
+
+// Covers 4.8.5.1 Hashmap definition
+export type ServerVariablesMap = {
+  [key in string]: ServerVariable;
+};
+
+// Covers 4.8.5.1
+export class Server {
+  private _url: string;
+  private _description: string;
+  private _variables: ServerVariablesMap;
+
+  constructor() {
+    this._variables = {};
+  }
+
+  public getUrl = (): string => this._url;
+  public getDescription = (): string => this._description;
+  public getVariables = (): ServerVariablesMap => this._variables;
+
+  public setUrl = (url: string) => (this._url = url);
+  public setDescription = (description: string) => (this._description = description);
+  public setVariables = (variables: ServerVariablesMap) => (this._variables = variables);
+}
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,3 +1,5 @@
 export * from './Contact';
 export * from './Info';
 export * from './License';
+export * from './ServerVariable';
+export * from './Server';
