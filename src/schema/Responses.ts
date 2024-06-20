a99b27d775dfe52cd7db10e96966882136f2bac4
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-19 20:05:24 -0600

    #25: creates responses schema object

--- src/schema/Responses.ts
+++ src/schema/Responses.ts
@@ -1,2 +1,21 @@
+import { Reference, Response } from '.';
+
 // Covers 4.8.16.1
-export class Responses {}
+export class Responses {
+  private _default: Response | Reference;
+  private _statusCode: string;
+  private _response: Response | Reference;
+
+  constructor() {
+    this._default = null;
+    this._response = null;
+  }
+
+  public getDefault = (): Response | Reference => this._default;
+  public getStatusCode = (): string => this._statusCode;
+  public getResponse = (): Response | Reference => this._response;
+
+  public setDefault = (_default: Response | Reference) => (this._default = _default);
+  public setStatusCode = (statusCode: string) => (this._statusCode = statusCode);
+  public setResponse = (response: Response | Reference) => (this._response = response);
+}
