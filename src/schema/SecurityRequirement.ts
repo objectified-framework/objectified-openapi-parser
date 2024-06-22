34ae93471635f0c6815edd21f28b8b6ebef15217
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-21 21:29:14 -0600

    #66: implements parse

--- src/schema/SecurityRequirement.ts
+++ src/schema/SecurityRequirement.ts
@@ -10,6 +10,12 @@ export class SecurityRequirement {
   public parse(segment: any): SecurityRequirement {
     const obj = new SecurityRequirement();
 
+    const key = Object.keys(segment)[0];
+    const value = segment[key];
+
+    obj.setName(key);
+    obj.setDeclarations(value);
+
     return obj;
   }
 
@@ -18,4 +24,8 @@ export class SecurityRequirement {
 
   public setName = (name: string) => (this._name = name);
   public setDeclarations = (declarations: string[]) => (this._declarations = declarations);
+
+  toString() {
+    return `[SecurityRequirement] _name=${this._name} _declarations=${this._declarations}`;
+  }
 }
