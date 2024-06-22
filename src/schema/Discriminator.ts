ed281fadd8f2d97fcf82c35c59eea9eeffe8f4a6
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-21 21:33:54 -0600

    #45: implements parse

--- src/schema/Discriminator.ts
+++ src/schema/Discriminator.ts
@@ -1,4 +1,6 @@
 // Covers 4.8.25.1 Hashmap
+import { ParsingError } from '../ParsingError';
+
 export type DiscriminatorMap = {
   [key in string]: string;
 };
@@ -15,6 +17,14 @@ export class Discriminator {
   public parse(segment: any): Discriminator {
     const obj = new Discriminator();
 
+    if (!segment['propertyName']) {
+      throw new ParsingError('Discriminator segment is missing required "propertyName"');
+    }
+
+    obj.setPropertyName(segment['propertyName']);
+
+    segment['mapping'].forEach((value, key) => (obj.getMapping()[key] = value));
+
     return obj;
   }
 
@@ -23,4 +33,8 @@ export class Discriminator {
 
   public setPropertyName = (propertyName: string) => (this._propertyName = propertyName);
   public setMapping = (mapping: DiscriminatorMap) => (this._mapping = mapping);
+
+  toString() {
+    return `[Discriminator] _propertyName=${this._propertyName} _mapping=${this._mapping}`;
+  }
 }
