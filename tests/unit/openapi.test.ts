2cfb4467382195eddfc71d86237bc1aaaafe06db
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-22 21:37:11 -0600

    set to ignore openapi

--- tests/unit/openapi.test.ts
+++ tests/unit/openapi.test.ts
@@ -3,4 +3,9 @@ import {OpenAPI} from '../../dist/schema';
 describe('OpenAPI OpenAPI Segment', () => {
   const fs = require('fs');
   const yaml = require('yaml');
+
+  it('should ignore tests for now', () => {
+
+  });
+
 });
