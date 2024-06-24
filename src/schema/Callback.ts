d05abb6545a30a9cadae2597a4dbb94314163203
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-24 17:54:06 -0600

    #82: implements documentation

--- src/schema/Callback.ts
+++ src/schema/Callback.ts
@@ -4,7 +4,17 @@ export type CallbackOrReferenceMap = {
   [key in string]: Callback | Reference;
 };
 
-// Covers 4.8.18.1
+/**
+ * Callback is a section of the OpenAPI that maps a possible out-of-bound callback related to the parent operation.
+ * Each value in the map is a ` Path Item` object that describes a set of requests that may be initiated by the API
+ * provider and the expected responses.  THe key value used to identify the path item object is an expression,
+ * evaluated at runtime, that identifies a URL to use for the callback operation.
+ *
+ * To describe incoming requests from the API provider independent from another API call, use the `webhooks` field.
+ *
+ * {@link https://spec.openapis.org/oas/latest.html#callback-object}
+ */
+
 export class Callback {
   private _expression: string;
   private _value: PathItem | Reference;
@@ -35,10 +45,16 @@ export class Callback {
     return obj;
   }
 
+  /** Retrieves the associated expression. */
   public getExpression = (): string => this._expression;
+
+  /** Retrieves the `PathItem` or `Reference` associated with the expression. */
   public getValue = (): PathItem | Reference => this._value;
 
+  /** Sets the expression. */
   public setExpression = (expression: string) => (this._expression = expression);
+
+  /** Assigns a `PathItem` object or a `Reference` to one, used to define a callback request and expected responses. */
   public setValue = (value: PathItem | Reference) => (this._value = value);
 
   toString() {
