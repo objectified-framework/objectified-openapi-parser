34e1bd42112067e94a3c5cb4c8868cd006bde4aa
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-24 18:05:55 -0600

    #83: implements documnentation.

--- src/schema/Discriminator.ts
+++ src/schema/Discriminator.ts
@@ -5,7 +5,16 @@ export type DiscriminatorMap = {
   [key in string]: string;
 };
 
-// Covers 4.8.25.1
+/**
+ * Discriminator is a section of the OpenAPI that is used when request bodies or response payloads may be one of a
+ * number of different schemas.  This object aids in serialization, deserialization, and validation.  The discriminator
+ * is a specific object in a schema which is used to inform the consumer of the document of an alternative schema
+ * based on the value associated with it.
+ *
+ * When using the discriminator, _inline_ schemas will not be considered.
+ *
+ * {@link https://spec.openapis.org/oas/latest.html#discriminator-object}
+ */
 export class Discriminator {
   private _propertyName: string; // Required
   private _mapping: DiscriminatorMap;
@@ -40,10 +49,16 @@ export class Discriminator {
     return obj;
   }
 
+  /** Retrieves the name of the property. */
   public getPropertyName = (): string => this._propertyName;
+
+  /** Retrieves the the mapping of payloads and schema names or references. */
   public getMapping = (): DiscriminatorMap => this._mapping;
 
+  /** _*REQUIRED*_.  Sets the name of the property in the payload that will hold the discriminator value. */
   public setPropertyName = (propertyName: string) => (this._propertyName = propertyName);
+
+  /** Sets the object to hold mappings between payload values and schema names or references. */
   public setMapping = (mapping: DiscriminatorMap) => (this._mapping = mapping);
 
   toString() {
