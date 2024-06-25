df47304a42c05c076f9cb8dba9c3935238aabac4
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-24 19:21:05 -0600

    #94: implements documentation.

--- src/schema/Schema.ts
+++ src/schema/Schema.ts
@@ -5,7 +5,18 @@ export type SchemaMap = {
   [key in string]: Schema;
 };
 
-// Covers 4.8.24.1
+/**
+ * Schema is a section of the OpenAPI that defines the input and output data types.  These types can be objects, but
+ * also primitives and arrays.  The object is a superset of the JSON Schema Specification Draft 2020-12.
+ *
+ * For more information about the properties, see JSON Schema Core and JSON Schema Validation.
+ *
+ * Unless stated otherwise, the property definitions follow those of JSON Schema and do not add any additional
+ * semantics.  Where JSON Schema indicates that behavior is defined by the application (e.g. for annotations), OpenAPI
+ * Specifications also defers the definition of semantics to the application consuming the OpenAPI document.
+ *
+ * {@link https://spec.openapis.org/oas/latest.html#schema-object}
+ */
 export class Schema {
   private _schema: any; // Required, this is a JSON Schema
   private _discriminator: Discriminator;
@@ -54,16 +65,34 @@ export class Schema {
     return obj;
   }
 
+  /** Retrieves the JSON schema definition. */
   public getSchema = (): any => this._schema;
+
+  /** Retrieves the discriminator. */
   public getDiscriminator = (): Discriminator => this._discriminator;
+
+  /** Retrieves the XML definition. */
   public getXml = (): XML => this._xml;
+
+  /** Retrieves external documentation. */
   public getExternalDocs = (): ExternalDocumentation => this._externalDocs;
+
+  /** Retrieves the example provided. */
   public getExample = (): any => this._example;
 
+  /** Sets the schema defined, removing the `discriminator`, `xml`, `externalDocs`, and `example` keys. */
   public setSchema = (schema: any) => (this._schema = schema);
+
+  /** Sets the support for polymorphism. */
   public setDiscriminator = (discriminator: Discriminator) => (this._discriminator = discriminator);
+
+  /** Sets the XML definition that _MAY_ be used only on properties schemas. */
   public setXml = (xml: XML) => (this._xml = xml);
+
+  /** Sets additional external documentation for this schema. */
   public setExternalDocs = (externalDocs: ExternalDocumentation) => (this._externalDocs = externalDocs);
+
+  /** Sets a free-form example of an instance for this schema. */
   public setExample = (example: any) => (this._example = example);
 
   toString() {
