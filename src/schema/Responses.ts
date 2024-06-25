9a961a24c9afa2a023c8791d546453480d0faece
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-24 19:15:08 -0600

    #94: implements documentation.

--- src/schema/Responses.ts
+++ src/schema/Responses.ts
@@ -4,7 +4,21 @@ export type ResponsesOrReferenceMap = {
   [key in string]: Response | Reference;
 };
 
-// Covers 4.8.16.1
+/**
+ * Responses is a section of the OpenAPI that stores the expected responses of an operation.  The container maps an
+ * HTTP response code to the expected response.
+ *
+ * The documentation is not necessarily expected to cover all possible HTTP response codes because they may not be known
+ * in advance.  However, documentation is expected to cover a successful operation response and any known errors.
+ *
+ * The `default` _MAY_ be used as a default response object for all HTTP codes that are not covered individually by
+ * the `Responses` object.
+ *
+ * The `Responses` object _MUST_ contain at least one response code, and if only one response code is provided, it
+ * _SHOULD_ be the response for a successful operation call.
+ *
+ * {@link https://spec.openapis.org/oas/latest.html#responses-object}
+ */
 export class Responses {
   private _default: Response | Reference;
   private _responses: ResponsesOrReferenceMap;
@@ -46,10 +60,16 @@ export class Responses {
     return obj;
   }
 
+  /** Retrieves the default response or reference. */
   public getDefault = (): Response | Reference => this._default;
+
+  /** Retrieves a list of all possible responses. */
   public getResponses = (): ResponsesOrReferenceMap => this._responses;
 
+  /** Sets the default response or reference to a response schema object. */
   public setDefault = (_default: Response | Reference) => (this._default = _default);
+
+  /** Sets a mapping of the possible response codes and their responses or references. */
   public setResponses = (responses: ResponsesOrReferenceMap) => (this._responses = responses);
 
   toString() {
