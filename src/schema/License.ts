d9fbcb8b93835142491966afec6419450985f34a
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-24 18:18:03 -0600

    #87: implements documentation.

--- src/schema/License.ts
+++ src/schema/License.ts
@@ -1,6 +1,10 @@
-// Covers 4.8.4.1
 import { ParsingError } from '.';
 
+/**
+ * Info is a section of the OpenAPI that provides license information for the exposed API.
+ *
+ * {@link https://spec.openapis.org/oas/latest.html#license-object}
+ */
 export class License {
   private _name: string; // Required
   private _identifier: string;
@@ -28,12 +32,22 @@ export class License {
     return obj;
   }
 
+  /** Retrieves the license name. */
   public getName = (): string => this._name;
+
+  /** Retrieves the identifier. */
   public getIdentifier = (): string => this._identifier;
+
+  /** Retrieves the URL. */
   public getUrl = (): string => this._url;
 
+  /** _*REQUIRED*_.  Sets the license name used for the API. */
   public setName = (name: string) => (this._name = name);
+
+  /** Sets an SPDX license expression for the API.  The `identifier` field is mutually exclusive for the `url` field. */
   public setIdentifier = (identifier: string) => (this._identifier = identifier);
+
+  /** Sets a URL to the license used for the API.  This _MUST_ be in the form of a URL. */
   public setUrl = (url: string) => (this._url = url);
 
   toString() {
