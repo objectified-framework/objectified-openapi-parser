893aba3f4cadf955903aaf7964f200e3b5f960c0
Ken Suenobu <ksuenobu@fastmail.com>
2024-06-24 18:01:53 -0600

    #83: implements documentation.

--- src/schema/Components.ts
+++ src/schema/Components.ts
@@ -24,7 +24,13 @@ import {
   PathItem,
 } from './sub';
 
-// Covers 4.8.7.1
+/**
+ * Components is a section of the OpenAPI that holds a set of reusable objects for different aspects of the OpenAPI
+ * Specification.  All objects defined within the components object will have no effect on the API unless they are
+ * explicitly references from properties outside the components object.
+ *
+ * {@link https://spec.openapis.org/oas/latest.html#components-object}
+ */
 export class Components {
   private _schemas: SchemaMap;
   private _responses: ResponseOrReferenceMap;
@@ -170,26 +176,64 @@ export class Components {
     return obj;
   }
 
+  /** Retrieves the schema map. */
   public getSchemas = (): SchemaMap => this._schemas;
+
+  /** Retrieves the responses map. */
   public getResponses = (): ResponseOrReferenceMap => this._responses;
+
+  /** Retrieves the parameters map. */
   public getParameters = (): ParameterOrReferenceMap => this._parameters;
+
+  /** Retrieves the examples map. */
   public getExamples = (): ExampleOrReferenceMap => this._examples;
+
+  /** Retrieves the request bodies map. */
   public getRequestBodies = (): RequestBodyOrReferenceMap => this._requestBodies;
+
+  /** Retrieves the headers map. */
   public getHeaders = (): HeaderOrReferenceMap => this._headers;
+
+  /** Retrieves the security schemes map. */
   public getSecuritySchemes = (): SecuritySchemeOrReferenceMap => this._securitySchemes;
+
+  /** Retrieves the links map. */
   public getLinks = (): LinkOrReferenceMap => this._links;
+
+  /** Retrieves the callbacks map. */
   public getCallbacks = (): CallbackOrReferenceMap => this._callbacks;
+
+  /** Retrieves the path items map. */
   public getPathItems = (): PathItemOrReferenceMap => this._pathItems;
 
+  /** Sets a map that holds reusable `Schema` objects. */
   public setSchemas = (schemas: SchemaMap) => (this._schemas = schemas);
+
+  /** Sets a map that holds reusable `Response` objects. */
   public setResponses = (responses: ResponseOrReferenceMap) => (this._responses = responses);
+
+  /** Sets a map that holds reusable `Parameter` objects. */
   public setParameters = (parameters: ParameterOrReferenceMap) => (this._parameters = parameters);
+
+  /** Sets a map that holds reusable `Example` or `Reference` objects. */
   public setExamples = (examples: ExampleOrReferenceMap) => (this._examples = examples);
+
+  /** Sets a map that holds reusable `RequestBody` or `Reference` objects. */
   public setRequestBodies = (requestBodies: RequestBodyOrReferenceMap) => (this._requestBodies = requestBodies);
+
+  /** Sets a map that holds reusable `Header` or `Reference` objects. */
   public setHeaders = (headers: HeaderOrReferenceMap) => (this._headers = headers);
+
+  /** Sets a map that holds reusable `SecurityScheme` or `Reference` objects. */
   public setSecuritySchemes = (securitySchemes: SecuritySchemeOrReferenceMap) => (this._securitySchemes = securitySchemes);
+
+  /** Sets a map that holds reusable `Link` or `Reference` objects. */
   public setLinks = (links: LinkOrReferenceMap) => (this._links = links);
+
+  /** Sets a map that holds reusable `Callback` or `Reference` objects. */
   public setCallbacks = (callbacks: CallbackOrReferenceMap) => (this._callbacks = callbacks);
+
+  /** Sets a map that holds reusable `PathItem` or `Reference` objects. */
   public setPathItems = (pathItems: PathItemOrReferenceMap) => (this._pathItems = pathItems);
 
   toString() {
